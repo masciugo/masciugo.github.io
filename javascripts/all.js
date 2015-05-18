57741f63db32edc8003731567a343f4d090b7701
masciugo <masciugo@gmail.com>
2015-05-18 12:58:47 +0200

    tech

--- javascripts/all.js
+++ javascripts/all.js
@@ -1199,7 +1199,13 @@ $('body').scrollspy({
 ;
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover',
+    placement: function () {
+      var placements = ["top", "bottom", "left", "right"]
+      return placements[Math.floor(Math.random() * placements.length)];
+    }
+  });
 })
 ;
 
--- javascripts/masciugo.js
+++ javascripts/masciugo.js
@@ -1,5 +1,11 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover',
+    placement: function () {
+      var placements = ["top", "bottom", "left", "right"]
+      return placements[Math.floor(Math.random() * placements.length)];
+    }
+  });
 })
 ;
