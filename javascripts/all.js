1a9218967d49670e1173fa7cf4735a7afe71d9a1
Corrado Masciullo <masciugo@gr-toniolo-iMac5.local>
2015-05-21 17:37:06 +0200

    better responsive

--- javascripts/all.js
+++ javascripts/all.js
@@ -208,7 +208,7 @@ $(function () {
   $('[data-toggle="popover"]').popover({
     trigger: 'hover',
     placement: function () {
-      var placements = ["top", "bottom", "left", "right"]
+      var placements = ["top", "bottom"]
       return placements[Math.floor(Math.random() * placements.length)];
     }
   });
